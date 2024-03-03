d3644b63bee0b88c93b1f62ca67fde9f780f2a51
Botxgojo <161952982+Botxgojo@users.noreply.github.com>
2024-03-03 21:32:40 +0100

    Update سرقه.js

--- plugins/سرقه.js
+++ plugins/سرقه.js
@@ -1,3 +1,5 @@
+import { addExif } from '../lib/sticker.js'
+
 let handler = async (m, { conn, text }) => {
     if (!m.quoted) throw 'الرجاء الاشارة الى الملصق الذي تريد أن تغير حقوقه مثال : \n *.سرقه packname | author*'
     
@@ -13,16 +15,22 @@ let handler = async (m, { conn, text }) => {
         
         stiker = await addExif(img, packname || global.packname, author || global.author)
         // Remove "bobiza" attribution
-        stiker = await addExif(stiker, packname || global.packname, author || global.author, '🥷')
+        stiker = await addExif(img, packname || global.packname, author || global.author, '🥷')
     } catch (e) {
         console.error(e)
         if (Buffer.isBuffer(e)) stiker = e
     } finally {
         if (stiker) {
             conn.sendFile(m.chat, stiker, 'سرقه.webp', '', m, false, { asSticker: true })
-            conn.react(m.chat, m.id, '🥷') // Add reaction
+            conn.sendButton(m.chat, 'الستيكر جاهز! 🥷', '©️ Powered by gojo bot', 'سرقه', m)
         } else {
             throw '*راسل صاحب البوت هناك مشكلة\n instagram.com/noureddine_ouafy*'
         }
     }
 }
+
+handler.help = ['سرقه']
+handler.tags = ['سرقه']
+handler.command = /^take|سرقه$/i
+
+export default handler
