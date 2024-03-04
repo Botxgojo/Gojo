f4a3a13a767e58d359dfcdcadbd0c4ac98d90c3d
Botxgojo <161952982+Botxgojo@users.noreply.github.com>
2024-03-04 12:20:25 +0100

    Update سرقه.js

--- plugins/سرقه.js
+++ plugins/سرقه.js
@@ -1,23 +1,27 @@
 import { addExif } from '../lib/sticker.js'
+
 let handler = async (m, { conn, text }) => {
-if (!m.quoted) throw 'الرجاء الاشارة الى الملصق الذي تريد أن تغير حقوقه مثال : \n *.سرقه gojo bot*'
-let stiker = false
-try {
-let [packname, ...author] = text.split('|')
-author = (author || []).join('|')
-let mime = m.quoted.mimetype || ''
-if (!/webp/.test(mime)) throw '*الرجاء الاشارة الى الملصق الذي تريد أن تغير حقوقه مثال : \n *.سرقه gojo bot*'
-let img = await m.quoted.download()
-if (!img) throw '*الرجاء  الاشارة الى الملصق الذي تريد أن تغير حقوقه مثال : \n *.سرقه gojo bot*'
-stiker = await addExif(img, packname || global.packname, author || global.author)
-} catch (e) {
-console.error(e)
-if (Buffer.isBuffer(e)) stiker = e
-} finally {
-if (stiker) conn.sendFile(m.chat, stiker, 'سرقه.webp', '', m, false, { asSticker: true })
-else throw '*راسل صاحب البوت هناك مشكلة\n instagram.com/noureddine_ouafy*'
-}}
+    if (!m.quoted) throw 'الرجاء الاشارة الى الملصق الذي تريد أن تغير حقوقه مثال : \n *.سرقه gojo bot*'
+    let stiker = false
+    try {
+        let [packname, ...author] = text.split('|')
+        author = (author || []).join('|')
+        let mime = m.quoted.mimetype || ''
+        if (!/webp/.test(mime)) throw '*الرجاء الاشارة الى الملصق الذي تريد أن تغير حقوقه مثال : \n *.سرقه gojo bot*'
+        let img = await m.quoted.download()
+        if (!img) throw '*الرجاء  الاشارة الى الملصق الذي تريد أن تغير حقوقه مثال : \n *.سرقه gojo bot*'
+        stiker = await addExif(img, packname || global.packname, author || global.author.replace("bobiza", ""))
+    } catch (e) {
+        console.error(e)
+        if (Buffer.isBuffer(e)) stiker = e
+    } finally {
+        if (stiker) conn.sendFile(m.chat, stiker, 'سرقه.webp', '', m, false, { asSticker: true })
+        else throw '*راسل صاحب البوت هناك مشكلة\n instagram.com/noureddine_ouafy*'
+    }
+}
+
 handler.help = ['سرقه']
 handler.tags = ['سرقه']
 handler.command = /^take|سرقه$/i
+
 export default handler
